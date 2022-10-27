9cd5227bb8a86142f5a05328ce0724b5051d5df6
okezino <okezi002@gmail.com>
2022-10-27 22:11:31 +0100

    completed

--- app/src/main/java/com/example/ktlintdemostration/MainActivity.kt
+++ app/src/main/java/com/example/ktlintdemostration/MainActivity.kt
@@ -3,19 +3,15 @@ package com.example.ktlintdemostration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ktlintdemostration.ui.theme.KtlintDemostrationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {//settffff
+        setContent { // settffff
             KtlintDemostrationTheme {
                 // A surface container using the 'background' color from the theme
             }
