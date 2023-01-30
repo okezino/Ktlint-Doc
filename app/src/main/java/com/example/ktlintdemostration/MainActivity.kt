dfbba643be9e97cca2e4b32a7e6ce74f1cafa8af
okezino <okezi002@gmail.com>
2023-01-30 17:21:18 +0100

    update build

--- app/src/main/java/com/example/ktlintdemostration/MainActivity.kt
+++ app/src/main/java/com/example/ktlintdemostration/MainActivity.kt
@@ -7,10 +7,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ktlintdemostration.ui.theme.KtlintDemostrationTheme
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCenter.start(
+            application, "240e5c49-370c-4d7e-8fa9-15188d3e0f81",
+            Analytics::class.java, Crashes::class.java
+        )
         setContent { // settffff
             KtlintDemostrationTheme {
                 // A surface container using the 'background' color from the theme
